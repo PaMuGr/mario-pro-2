d880b74f3e95632b0786d1427f79c71c99e06e4c
Pau <pau.muras@gmail.com>
2025-05-14 12:20:51 +0200

    Changes to doxyfile

--- finder.hh
+++ finder.hh
@@ -2,21 +2,76 @@
  * @brief Especificació de les Funcions del Finder
  */
 
+#include <map>
+#include <set>
+#include "utils.hh"
+
+
 template <typename T>
 class Finder {
-    //mapa
+
+private:
+
+    //bastant segur de la primera estructura, la segona es una proba per veure si funcionaria
+    std::map<const T*, pro2::Rect> objecteRectangle;
+
+    std::map<pro2::Rect, const T*> rectanglesObjectes;
+
 public:
     Finder() {}
 
     //n vegades
-    void add(const T *t);
+    void add(const T *t){
+        pro2::Rect rect = t->get_rect();
+        objecteRectangle[t] = rect;
+        rectanglesObjectes[rect] = t;
+    }
 
     //cada frame: n vegades
-    //Find amb map i actualitzar el segon element del map
-    void update(const T *t);
+    //
+    //Find amb map i actualitzar el segon element del map?
+    //Guardem abans d'actualitzar i canviem primer element del segon map?
+    void update(const T *t){
+
+        auto it = objecteRectangle.find(t);
+        //si existeix intercanviem
+        if(it != objecteRectangle.end()){
+
+            pro2::Rect rect = t->get_rect();
+
+            //busquem al segon map el rectangle que haurem d'intercanviar
+            auto it2 = rectanglesObjectes.find(it -> second);
+            //mirem si existeix i si coincideix amb el iterador buscat
+            if(it2 != rectanglesObjectes.end() && it2->second == t){
+                rectanglesObjectes.erase(it2);
+                //insertem després d'eliminar-ho amb el nou rectangle i mateix iterador
+                rectanglesObjectes.insert({rect,t});
+            }
+
+            //canviat el rect del primer map per el que toca
+            it -> second = rect;
+        }
+
+    }
 
     //quan l'agafes
-    void remove(const T *t);
+    void remove(const T *t){
+        auto it = objecteRectangle.find(t);
+        //si existeix hem d'eliminar-lo i per aixo borrem el punter
+        if (it != objecteRectangle.end()) {
+            //guardem el rectangle que buscarem en el segon map
+            pro2::Rect rect = it->second;
+            //borrem
+            objecteRectangle.erase(it); 
+
+            auto it2 = rectanglesObjectes(rect);
+            //mirem si existeix i si coincideix amb el iterador buscat
+            if(it2 != rectanglesObjectes.end() && it2->second == t){
+                //esborrem
+                rectanglesObjectes.erase(it2);
+            }
+        }
+    }
 
     /**
      * @brief Retorna el conjunt d'objectes amb rectangles 
